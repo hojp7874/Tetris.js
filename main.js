551bcd064dceab56da384b53934bdd59571b17ed
heotaeyun <heotaeyun8136@gmail.com>
2020-10-29 17:54:35 +0900

    add board

---
+++ main.js
@@ -0,0 +1,49 @@
+const canvas = document.querySelector('#board')
+const ctx = canvas.getContext('2d')
+
+// 상수를 사용해 캔버스의 크기를 계산한다.
+ctx.canvas.width = COLS * BLOCK_SIZE
+ctx.canvas.height = ROWS * BLOCK_SIZE
+
+// 블록의 크기를 변경한다.
+ctx.scale(BLOCK_SIZE, BLOCK_SIZE)
+
+let board = new Board();
+
+function play() {
+  board.getEmptyBoard();
+  let piece = new Piece(ctx);
+  piece.draw();
+  
+  board.piece = piece;
+}
+
+// ...이 뭐야?
+moves = {
+  [KEY.LEFT]:  p => ({ ...p, x: p.x - 1 }),
+  [KEY.RIGHT]: p => ({ ...p, x: p.x + 1 }),
+  [KEY.DOWN]:  p => ({ ...p, y: p.y + 1 })
+};
+
+// 이걸 써야 원본 조각을 변형시키지 않고 새로운 상태를 얻는대.
+// const p = this.moves[event.key](this.piece)
+
+document.addEventListener('keydown', event => {
+  if (moves[event.keyCode]) {  
+    // 이벤트 버블링을 막는다.
+    event.preventDefault();
+    
+    // 조각의 새 상태를 얻는다.
+    let p = moves[event.keyCode](board.piece);
+    
+    if (board.valid(p)) {    
+      // 이동이 가능한 상태라면 조각을 이동한다.
+      board.piece.move(p);
+      
+      // 그리기 전에 이전 좌표를 지운다.
+      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height); 
+      
+      board.piece.draw();
+    }
+  }
+});
